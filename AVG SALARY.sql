0b861fd8850daf4de60e3e5c6e4aeb93f8e98067
Josh600x <155986252+Josh600x@users.noreply.github.com>
2024-01-08 14:30:44 +0000

    Update AVG SALARY.sql

--- AVG SALARY.sql
+++ AVG SALARY.sql
@@ -1,8 +1,5 @@
+/* This code helps determine avg salary across departments and gender*/
 
-Drop procedure if exists filter_salary;
-DELIMITER $$
-Create procedure  filter_salary (in min_salary FLOAT, in max_salary FLOAT)
-begin
 Select e.gender,d.dept_name,avg(s.salary) as AVG_Salary
 from 
 t_salaries s
@@ -12,11 +9,7 @@ Join
 t_dept_emp de on e.emp_no=de.emp_no
 Join
 t_departments d on de.dept_no=d.dept_no
-where s.salary between min_salary and max_salary
 group by d.dept_no, e.gender;
-End$$
-
-CALL filter_salary(50000, 90000);
 
 
 
